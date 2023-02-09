b779ab3fc149ac7b6ff165b64c616de710f67a3d
Vinícius Lacerda <viniciuslmn@gmail.com>
2023-02-09 10:26:14 -0300

    feat: error client

---
+++ backend/src/middleware/error.middleware.ts
@@ -0,0 +1,17 @@
+import { ErrorRequestHandler, NextFunction, Request, Response } from 'express';
+import ErrorClient from '../utils/ErrorClient';
+
+const errorMiddleware: ErrorRequestHandler = (
+  err: ErrorRequestHandler,
+  _req: Request,
+  res: Response,
+  _next: NextFunction,
+): Response => {
+  if (err instanceof ErrorClient) {
+    return res.status(err.statusCode).json({ message: err.message });
+  }
+
+  return res.status(500).json({ message: 'Internal error' });
+};
+
+export default errorMiddleware;
