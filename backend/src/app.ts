0e4b01959e21b5cdf601e6b687006c1680eb2eb5
Vinícius Lacerda <viniciuslmn@gmail.com>
2023-02-09 08:55:20 -0300

    initial setup

---
+++ backend/src/app.ts
@@ -0,0 +1,15 @@
+import * as express from 'express';
+
+export default class App {
+  public app: express.Express;
+
+  constructor() {
+    this.app = express();
+
+    this.app.use(express.json());
+  }
+
+  public start(PORT: string | number):void {
+    this.app.listen(PORT, () => console.log(`Running on port ${PORT}`));
+  }
+}
