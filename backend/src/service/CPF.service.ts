84aa8df2ef633de5a7ad821ad75380ff6072ab7c
Vinícius Lacerda <viniciuslmn@gmail.com>
2023-02-09 10:24:44 -0300

    fix cpf service

--- backend/src/service/CPF.service.ts
+++ backend/src/service/CPF.service.ts
@@ -2,7 +2,8 @@ import CPFModel from '../database/models/CPF';
 
 export default class CPFService {
   public create =  async (cpf: string) => {
-    const newCPF = await CPFModel.create({ cpf });
+    const newCPF = await CPFModel.create({ cpf })
+      .then(({ cpf, createdAt }) => ({ cpf, createdAt }));
     return newCPF;
   }
 }
